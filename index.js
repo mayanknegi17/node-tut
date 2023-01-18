dd7b33606385a316bb23d894a7bd5cf02a1e75ae
Mayank Singh <mayank@turningcloud.in>
2023-01-18 23:02:05 +0530

    http request in node js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const app = require('./app');
-console.log(app.add())
+const http = require('http');
+// console.log(app.add())
+
+function dataControle(req, resp) {
+    resp.write("<h1>This is the changed ok</h1>");
+    resp.end();
+}
+
+// arrow Function
+
+// const dataControle = (req, resp) => {
+//     resp.write("<h1>This is the changed ok</h1>");
+//     resp.end();
+// }
+
+http.createServer(dataControle) .listen(4500);
